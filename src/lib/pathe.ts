ceff428e7ec121427d035d2a082602d07a9726ad
Thinh Nguyen <thinhnguyenhuuhung@gmail.com>
2024-02-11 17:54:53 +0100

    [UPDATE] switched to nuxt

--- src/lib/pathe.ts
+++
@@ -1,5 +0,0 @@
-const FILENAME_RE = /(^|[/\\])([^/\\]+?)(?=(\.[^.]+)?$)/;
-
-export function filename(path: string) {
-  return path.match(FILENAME_RE)?.[2];
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      'white': "#F2EFE5",
+      "black": "#141415",
+      "dim-gray": "#141415",
+      "gray": "#141415",
+      "light-gray": "#C7C8CC"
+    }
   },
   plugins: [],
 }
