6962dc02dc82fcb9d145819159a5ab65c1d58978
Thinh Nguyen <thinhnguyenhuuhung@gmail.com>
2024-02-12 15:03:22 +0100

    [UPDATE] fixed some nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  nitro: {
+    compressPublicAssets: true,
+  },
   modules: ["@nuxt/image", "@nuxtjs/tailwindcss"],
   css: ["~/assets/global.css"],
   image: {
