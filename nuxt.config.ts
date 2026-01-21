04d1856d1047670115003cde608f8a84dd1c6647
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2026-01-21 18:22:22 +0100

    [UPDATE]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,57 +35,46 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
-          hid: 'description',
           name: 'description',
           content: description
         },
         {
-          hid: 'fb:type',
           property: 'fb:type',
           content: domain
         },
         {
-          hid: 'og:site_name',
           property: 'og:site_name',
           content: title
         },
         {
-          hid: 'og:url',
           property: 'og:url',
           content: domain
         },
         {
-          hid: 'og:title',
           property: 'og:title',
           content: title
         },
         {
-          hid: 'twitter:title',
           property: 'twitter:title',
           content: title
         },
         {
-          hid: 'og:description',
           property: 'og:description',
           content: description
         },
         {
-          hid: 'twitter:description',
-          property: 'og:description',
+          property: 'twitter:description',
           content: description
         },
         {
-          hid: 'og:image',
           property: 'og:image',
           content: 'https://www.thinis.de/_vercel/image?url=%2Fsection%2Ffinal-1-2.webp&w=1280&q=80',
         },
         {
-          hid: 'twitter:image',
-          property: 'og:image',
+          property: 'twitter:image',
           content: 'https://www.thinis.de/_vercel/image?url=%2Fsection%2Ffinal-1-2.webp&w=1280&q=80',
         },
         {
-          hid: 'robots',
           name: 'robots',
           content: 'index, follow'
         },
@@ -99,8 +88,6 @@ export default defineNuxtConfig({
         }
       ],
       title: "Thinh Nguyen | Photography",
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
     }
   },
 })
